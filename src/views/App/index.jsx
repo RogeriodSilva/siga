fcd80e9f9f79e1707cc8ca19951dada90cfa7534
Rogério S Junior <ro.juniorkk@gmail.com>
2026-08-26 17:32:06 -0300

    first teste

--- src/views/App/index.jsx
+++ src/views/App/index.jsx
@@ -1,25 +1,60 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import FinalizarEventos from "../FinalizarEventos";
 import { BookCheck } from "lucide-react";
-import { useState } from "react";
+import { use, useState } from "react";
 import Loading from "../Loading";
 import useLocalStorageState from "use-local-storage-state";
 
 export default function App() {
-      const [local, setLocal] = useLocalStorageState("localStorage", {
-            defaultValue: {},
-      });
+      const [items, setItems] = useState(["Comprar leite", "Estudar React"]);
+      const [line, setLine] = useState("");
 
-      chrome.tabs.onActivated.addListener(async ({ tabId }) => {
-            const tab = await chrome.tabs.get(tabId);
-            setLocal({ tab });
-      });
+      const addItem = (item) => {
+            setItems([...items, item]);
+      };
+
+      const submitTarefa = () => {
+            if (line.trim() === "") return;
+            addItem(line);
+            setLine("");
+      };
 
       return (
-            <>
-                  <Loading></Loading>
-            </>
+            <div>
+                  <textarea
+                        value={line}
+                        onChange={(e) => setLine(e.target.value)}
+                  ></textarea>
+
+                  <button
+                        onClick={submitTarefa}
+                        className="mb-4 rounded-lg border px-4 py-2"
+                  >
+                        Adicionar
+                  </button>
+
+                  <div className="flex flex-col space-y-1">
+                        {items.map((e, index) => (
+                              <span key={index}>{e}</span>
+                        ))}
+                  </div>
+            </div>
       );
+
+      // const [local, setLocal] = useLocalStorageState("localStorage", {
+      //       defaultValue: {},
+      // });
+
+      // chrome.tabs.onActivated.addListener(async ({ tabId }) => {
+      //       const tab = await chrome.tabs.get(tabId);
+      //       setLocal({ tab });
+      // });
+
+      // return (
+      //       <>
+      //             <Loading></Loading>
+      //       </>
+      // );
 
       // const MenusList = [
       //       // { icon: <Home />, value: "home", render: <Welcome /> },
--- src/views/Loading/index.jsx
+++ src/views/Loading/index.jsx
@@ -1,9 +1,6 @@
 import { Spinner } from "@/components/ui/spinner";
-import useLocalStorageState from "use-local-storage-state";
 
 export default function Loading({ ...param }) {
-      const [local] = useLocalStorageState("localStorage");
-
       return (
             <div className="w-full h-100 flex flex-col space-y-4 items-center justify-center">
                   <Spinner className="size-8" />
